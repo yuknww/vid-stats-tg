d41d357ce7ece40247717f489a729bac303f5023
Тимофей Скачков <timoha425@icloud.com>
2026-02-03 18:19:52 +0300

    change db and main

--- app/db.py
+++ app/db.py
@@ -1,12 +1,32 @@
-import os
+import asyncio
+import asyncpg
 
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from config import DATABASE_URL
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = DATABASE_URL
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 Base = declarative_base()
 
+
+async def create_pool():
+    return await asyncpg.create_pool(DATABASE_URL)
+
+pool = asyncio.run(create_pool())
+
+async def execute_sql(sql: str) -> int:
+    """
+    Выполняет SQL и возвращает одно число.
+    Безопасность: только SELECT.
+    """
+    sql_lower = sql.strip().lower()
+    if not sql_lower.startswith("select"):
+        raise ValueError("Только SELECT разрешён!")
+
+    async with pool.acquire() as conn:
+        result = await conn.fetchval(sql)
+        return result or 0
--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("hello world")
+import asyncio
+
+from aiogram import Bot, Dispatcher
+
+from config import BOT_TOKEN
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+async def main():
+    try:
+        await dp.start_polling(bot)
+    finally:
+        await bot.session.close()
+
+if __name__ == "__main__":
+    asyncio.run(main())
