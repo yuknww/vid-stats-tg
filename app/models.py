b6eb9935fce244bb9a49fcb2eeb197e4aa80d492
Тимофей Скачков <timoha425@icloud.com>
2026-02-03 22:26:42 +0300

    upd models

--- app/models.py
+++ app/models.py
@@ -3,20 +3,20 @@ import datetime
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 
-from db import Base
+from app.db import Base
 
 class Video(Base):
     __tablename__ = "videos"
 
     id = Column(String, primary_key=True)
     creator_id = Column(String, nullable=False)
-    video_created_at = Column(DateTime, nullable=False)
+    video_created_at = Column(DateTime(timezone=True), nullable=False)
     views_count = Column(Integer, default=0)
     likes_count = Column(Integer, default=0)
     comments_count = Column(Integer, default=0)
     reports_count = Column(Integer, default=0)
-    created_at = Column(DateTime, default=datetime.datetime.now)
-    updated_at = Column(DateTime, default=datetime.datetime.now, onupdate=datetime.datetime.now)
+    created_at = Column(DateTime(timezone=True), default=datetime.datetime.now)
+    updated_at = Column(DateTime(timezone=True), default=datetime.datetime.now, onupdate=datetime.datetime.now)
 
     snapshots = relationship("VideoSnapshot", back_populates="video")
 
@@ -37,7 +37,7 @@ class VideoSnapshot(Base):
     delta_comments_count = Column(Integer, default=0)
     delta_reports_count = Column(Integer, default=0)
 
-    created_at = Column(DateTime, nullable=False)
-    updated_at = Column(DateTime, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow)
+    created_at = Column(DateTime(timezone=True), nullable=False)
+    updated_at = Column(DateTime(timezone=True), default=datetime.datetime.now, onupdate=datetime.datetime.now)
 
     video = relationship("Video", back_populates="snapshots")
