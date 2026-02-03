8ad08b26fd2a69b8853f6f14ddc979d6c1a8a966
Тимофей Скачков <timoha425@icloud.com>
2026-02-03 22:30:12 +0300

    change pool

--- app/db.py
+++ app/db.py
@@ -1,22 +1,17 @@
-import asyncio
 import asyncpg
-
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from config import DATABASE_URL
+from app.config import DATABASE_URL
 
 DATABASE_URL = DATABASE_URL
+ASYNC_PG_DATABASE_URL = DATABASE_URL.replace("+asyncpg", "")
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 Base = declarative_base()
 
-
-async def create_pool():
-    return await asyncpg.create_pool(DATABASE_URL)
-
-pool = asyncio.run(create_pool())
+pool: asyncpg.Pool | None = None
 
 async def execute_sql(sql: str) -> int:
     """
@@ -25,8 +20,13 @@ async def execute_sql(sql: str) -> int:
     """
     sql_lower = sql.strip().lower()
     if not sql_lower.startswith("select"):
-        raise ValueError("Только SELECT разрешён!")
+        raise ValueError("Неверный запрос")
 
     async with pool.acquire() as conn:
         result = await conn.fetchval(sql)
         return result or 0
+
+async def on_startup():
+    """ Создаёт пул соединений с БД """
+    global pool
+    pool = await asyncpg.create_pool(ASYNC_PG_DATABASE_URL)
