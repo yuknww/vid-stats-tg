68fe34611d58f543a54f1946f9c70879b65b89d9
Тимофей Скачков <timoha425@icloud.com>
2026-02-03 22:29:25 +0300

    add description, clean sql and logging

--- app/api.py
+++ app/api.py
@@ -1,11 +1,33 @@
+import logging
+import re
+
 from groq import AsyncGroq
 
-from config import API_KEY, SYSTEM_PROMPT
+from app.config import API_KEY, SYSTEM_PROMPT
 
+logger = logging.getLogger()
 client = AsyncGroq(api_key=API_KEY)
 
 
+def clean_sql(llm_output: str) -> str:
+    """
+    Убирает ```sql ... ``` и лишний текст
+    """
+    text = llm_output.strip()
+
+    if text.startswith("```"):
+        text = re.sub(r"^```[\w]*\n?", "", text)
+        text = re.sub(r"\n?```$", "", text)
+
+    return text.strip()
+
 async def llm_generate_sql(question: str) -> str:
+    """
+    Отправляет запрос в LLM и возвращает чистый SQL
+    :param question: вопрос пользователя
+    :return: sql код
+    """
+
     completion = await client.chat.completions.create(
         model="llama-3.3-70b-versatile",
         messages=[
@@ -14,4 +36,6 @@ async def llm_generate_sql(question: str) -> str:
         ],
         temperature=0
     )
-    return completion.choices[0].message.content.strip()
+    raw = completion.choices[0].message.content.strip()
+    logger.info(f"Ответ от ИИ: {raw}")
+    return clean_sql(raw)
